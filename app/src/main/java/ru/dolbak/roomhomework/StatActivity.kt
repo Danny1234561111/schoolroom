b8bbccc30db50b11c05b4e79a34a10bbfb61258a
Nikita Dolbak <nikitf28@yandex.ru>
2023-05-20 11:48:25 +0800

    Fixed

---
+++ app/src/main/java/ru/dolbak/roomhomework/StatActivity.kt
@@ -0,0 +1,11 @@
+package ru.dolbak.roomhomework
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stat)
+    }
+}
